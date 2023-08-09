e3c11defbd5327ef1975fd8b16a1cf183b9aeb1c
francogodoy92 <franck92elrojo@gmail.com>
2023-08-09 18:28:37 -0300

    Sprint1Ready

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -4,8 +4,8 @@ import "../index.css";
 function Cities() {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-900">
-        <img className="rounded-2xl" src='https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmZzbGMxc3hkaGFudWJmbDEwcjI0ZXphODkwdXVvdjBmcTRrZjUzZiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3oKIPnAiaMCws8nOsE/giphy-downsized.gif' alt="Página en construcción" title="En construcción" /> 
+      <div className="min-h-screen flex items-center justify-center ">
+        <img className="rounded-2xl shadow-lg" src='https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmZzbGMxc3hkaGFudWJmbDEwcjI0ZXphODkwdXVvdjBmcTRrZjUzZiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3oKIPnAiaMCws8nOsE/giphy-downsized.gif' alt="Página en construcción" title="En construcción" /> 
       </div>
     </>
   );
