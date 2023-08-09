d4edd76c3416253f166c2800858a29faa0536712
francogodoy92 <franck92elrojo@gmail.com>
2023-08-09 18:41:01 -0300

    Sprint1Ready

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -3,11 +3,14 @@ import "../index.css";
 
 function Cities() {
   return (
-    <>
-      <div className="min-h-screen flex items-center justify-center ">
-        <img className="rounded-2xl shadow-lg" src='https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmZzbGMxc3hkaGFudWJmbDEwcjI0ZXphODkwdXVvdjBmcTRrZjUzZiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3oKIPnAiaMCws8nOsE/giphy-downsized.gif' alt="Página en construcción" title="En construcción" /> 
+    <>      
+      <div className="min-h-screen flex items-center flex-col gap-3 justify-center mt-5 ">
+        <h1 className="font-bold text-left text-3xl bg-blue-950 text-blue-300 drop-shadow-xl p-3 rounded-md">
+          Under construction
+        </h1>
+        <img className="rounded-2xl shadow-lg" src='https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmZzbGMxc3hkaGFudWJmbDEwcjI0ZXphODkwdXVvdjBmcTRrZjUzZiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3oKIPnAiaMCws8nOsE/giphy-downsized.gif' alt="Página en construcción" title="En construcción" />
       </div>
-    </>
+    </> 
   );
 }
 
