e2435dda911efa2d1188d060623cf3e10d043e04
francogodoy92 <franck92elrojo@gmail.com>
2023-09-04 00:36:17 -0300

    Style Ready , creo

--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -1,54 +1,68 @@
 import { useState } from "react";
 
-const Itinerary = ({data}) => {
-
+const Itinerary = ({ data }) => {
   const [expanded, setExpanded] = useState(false);
-  
-  const price = []
+
+  const price = [];
   for (let i = 0; i < data.price; i++) {
-      price.push(i)
+    price.push(i);
   }
 
   return (
-      <div className="" >
-
-          <div className="flex flex-col">
-            <img src={data.photo} alt={data.name} className="w-[25wv] h-[10wv]"/>
-            <h1>{data.name}</h1>
-            <div className="tags">
-                {data.tematicHashtags.map((tag, index)=>{
-                    return(
-                        <p key={index}>{tag}</p>
-                    )
-                })}
+    <div className="text-blue-950 border-4 rounded-xl p-2">
+      <img
+        src={data.photo}
+        alt={data.name}
+        className="w-80 max-h-40 rounded-md shadow-md border object-cover"
+      />
+      <div className="flex flex-col gap-2">
+        <h1 className="text-xl font-bold text-center">{data.name}</h1>
+        <div className="flex align-center gap-2 items-center">
+          <img
+            src={data.authorPhoto}
+            alt={data.authorName}
+            className="w-10 h-10 rounded-full object-cover"
+          />
+          <h3 className="font-semibold text-xl text-black">
+            {data.authorName}
+          </h3>
+        </div>
+        <div className="flex justify-between items-center">
+          <p className="flex font-medium text-black ">
+            <span className="underline">Price:</span>
+            {price.map(() => {
+              return <p>💸</p>;
+            })}
+          </p>
+          <p className="font-bold text-2xl">/</p>
+          <p className="font-medium">
+            <span className="text-black underline">Duration:</span>{" "}
+            {data.duration}hs.
+          </p>
+        </div>
+        <div className="flex gap-2 font-medium text-blue-950">
+          {data.tematicHashtags.map((tag, index) => {
+            return <p key={index}>{tag}</p>;
+          })}
+        </div>
+        <div>
+          <p
+            onClick={() => {
+              setExpanded(!expanded);
+            }}
+            className=" cursor-pointer text-blue-200 bg-blue-950 text-center py-1 px-2 rounded-md hover:bg-blue-100 border border-blue-950 hover:text-blue-950 hover:shadow-lg w-auto text-lg"
+          >
+            View More
+          </p>
+          <div className={expanded ? "expanded" : "hidden"}>
+            <div>
+              <p className="text-center text-black font-medium">Coming soon</p>
             </div>
-              <div className="flex">
-                  <img src={data.authorPhoto} alt={data.authorName} className="w-5 h-5 rounded-full"/>
-                  <h3>{data.authorName}</h3>
-              </div>
-              <h5>Price:
-              {
-                  price.map(() => {
-                      return (
-                        <p>💸</p>
-                      )
-                  })
-              }   
-              </h5>
-              <h5>Duration: {data.duration}hs.</h5>
           </div>
-          <div>
-            <p onClick={()=>{setExpanded(!expanded)}} className=" cursor-pointer text-blue-200 bg-blue-950 text-center py-1 px-2 rounded-md hover:bg-blue-100 border border-blue-950 hover:text-blue-950 hover:shadow-lg w-auto text-lg">
-              View More
-            </p>
-            <div className={expanded ? 'expanded' : 'hidden'}>
-              <div>
-                  <h2>Coming soon</h2>
-              </div>
-          </div>
-          </div>
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Itinerary
+export default Itinerary;
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { readOneCity, readItinerariesByCity } from "../Redux/actions/citiesAction.js";
+import {
+  readOneCity,
+  readItinerariesByCity,
+} from "../Redux/actions/citiesAction.js";
 import Itinerary from "../components/Itinerary";
 import NoItineraries from "../components/NoItineraries";
 
@@ -11,24 +14,25 @@ function Details() {
 
   const city = useSelector((store) => store.readOneCityReducer.city);
 
-  const itineraries = useSelector((store) => store.readItinerariesByCityReducer.itineraries);
+  const itineraries = useSelector(
+    (store) => store.readItinerariesByCityReducer.itineraries
+  );
 
   useEffect(() => {
     dispatch(readOneCity(params.id));
     dispatch(readItinerariesByCity(params.id));
   }, []);
 
-
   return (
     <div className="min-h-[80vh]  px-20 py-5">
       <div className="flex flex-col gap-3 items-center">
         {city ? (
-          <div>
-            <p className="text-3xl text-center font-bold py-2">
+          <div className="mb-12">
+            <p className="text-3xl text-center font-bold py-2 text-blue-950">
               {city.city}, {city.country}
             </p>
             <img
-              className={` h-40 w-[100vw] object-cover rounded-lg shadow-2xl border-2 border-blue-950 `}
+              className={` h-60 w-96 object-cover rounded-lg shadow-2xl border-2 border-blue-950 `}
               src={city.photo}
               title={city.featuredLocation}
               alt={city.city}
@@ -37,15 +41,15 @@ function Details() {
         ) : (
           <p>Loading city details...</p>
         )}
-      <div className="">
-        {itineraries.length === 0 ? (
-          <NoItineraries /> 
-        ) : (
-          itineraries.map((itinerary) => {
-            return <Itinerary data={itinerary} key={itinerary.id} />;
-          })
-        )}
-      </div>
+        <div className="flex flex-col gap-5">
+          {itineraries.length === 0 ? (
+            <NoItineraries />
+          ) : (
+            itineraries.map((itinerary) => {
+              return <Itinerary data={itinerary} key={itinerary.id} />;
+            })
+          )}
+        </div>
       </div>
       <Link to="/Cities" className="flex items-end justify-end">
         <p className="text-blue-200 bg-blue-950 text-center py-1 px-2 rounded-md hover:bg-blue-100 border border-blue-950 hover:text-blue-950 hover:shadow-lg w-auto text-lg">
